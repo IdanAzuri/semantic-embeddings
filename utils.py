1c20e119010baa54acd4965f066c65674d0bc715
idanazuri <idan.azuri@gmail.com>
2020-01-18 17:23:59 +0200

    pretrained

--- utils.py
+++ utils.py
@@ -237,7 +237,7 @@ def build_network(num_outputs, architecture, classification = False, no_softmax 
             # ResNet50 has been available from the beginning, while the other two were added in keras-applications 1.0.7.
             # Thus, we use the initial implementation of ResNet50 for compatibility's sake.
             factory = keras.applications.ResNet50
-        rn = factory(include_top=False, weights=None, input_shape=input_shape)
+        rn = factory(include_top=False, weights='imagenet', input_shape=input_shape)
         # Depending on the Keras version, the ResNet50 model may or may not contain a final average pooling layer.
         rn_out = rn.layers[-2].output if isinstance(rn.layers[-1], keras.layers.AveragePooling2D) else rn.layers[-1].output
         x = keras.layers.GlobalAvgPool2D(name='avg_pool')(rn_out)
